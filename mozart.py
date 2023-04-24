a3f9bed68389f019d908bd744ab3483f5d690eac
Joe Paris <2662123+joeparis@users.noreply.github.com>
2023-04-24 11:34:54 -0700

    Make less "hacky"

--- mozart.py
+++ mozart.py
@@ -21,12 +21,12 @@ def get_selection(prompt: str, options: tuple, clear_screen=True) -> str:
 
     print(prompt)
     for idx, option in enumerate(options):
-        print(f"  {idx + 1}. {option.lower()}")
+        print(f"  {idx + 1}. {option.lower().replace('-', ' & ')}")
 
     print()
     selection = int(input("Please enter the number of your selection: ")) - 1
 
-    return options[selection].replace(" & ", "-")  # HACK
+    return options[selection]
 
 
 def clear():
@@ -36,7 +36,7 @@ def clear():
 
 def main():
     styles = ("minuet", "trio")
-    instruments = ("clarinet", "flute & harp", "mbira", "piano")
+    instruments = ("clarinet", "flute-harp", "mbira", "piano")
 
     style = get_selection("What style of composition would you like to create? ", styles)
     instrument = get_selection("What instrument would you like to use? ", instruments)
